9415b48a63063ae4667bd75737c6af9ae48fb3ea
Johny-zu <angel_outlook.com>
2024-04-07 20:37:37 -0600

    Clase para conocimiento de variables basicas

---
+++ src/Bases/Impresion.kt
@@ -0,0 +1,8 @@
+package Bases
+
+fun main() {
+    val a = "Maria"; //Variable de solo lectura
+    val b = 20; //Cuidado con compatibilidad de tipos de datos
+
+    println("$a tiene $b años de edad")
+}
--- src/Bases/Inicio.kt
+++ src/Bases/Inicio.kt
@@ -2,6 +2,4 @@ package Bases
 
 fun main() {
     println("Inicio del lenguaje") //Asi se hace el comienzo del codigo
-
-
 }
