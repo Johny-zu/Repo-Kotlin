3eaf5a8dfce31b0d84fc9119af667b7107e42097
Johny-zu <angel_outlook.com>
2024-04-07 20:45:46 -0600

    Conocemos como usar variables

---
+++ src/Bases/Variables.kt
@@ -0,0 +1,22 @@
+package Bases
+
+fun main() {
+    var clientes = 10; //Se inicializa valor mutable
+
+    clientes = 8 //odemos darle un valor de manera mas directa
+
+    clientes = clientes + 3; //asi podemos sumar clientes
+    clientes += 7 // sumamos clientes
+    clientes -= 3 //restamos clientes
+    clientes *= 2 //multiplicamos a los clientes
+    clientes /= 3 //dividimos a los clientes
+
+    println(clientes) //imprimiso clientes
+
+    var con : Int //Asi podemos hacer una variable sin inicializar
+    con = 3
+    val pal : String = "Hola" //otra forma
+
+    println(con)
+    println(pal)
+}
